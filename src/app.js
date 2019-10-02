f19e99a329dba789113ac4fa5140816ede949c67
marlondck <15050029+marlondck@users.noreply.github.com>
2019-10-02 15:44:32 -0400

    Rate Limit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import Youch from 'youch';
 // -> para ouvir o erros alem do sentry precisamos ouvir os erros que vem dos controllers que usam async
 // por padrao o express nao ouve.. dai usaremos a extensao abaixo
@@ -41,6 +44,21 @@ class App {
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+    // -> rate limit
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
